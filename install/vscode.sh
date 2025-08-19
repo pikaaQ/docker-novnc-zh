68db80bcaa7c77f785f8a8682e06cba3d524744b
mizzle <pikaaq@qq.com>
2025-08-19 08:30:09 +0000

    docs(install): 给安装脚本添加简说明.

--- install/vscode.sh
+++ install/vscode.sh
@@ -1,4 +1,5 @@
 apt install xdg-utils
 
+# code_1.103.0出现输入卡死，不支持google输入法的情况，1.102则没问题
 wget https://vscode.download.prss.microsoft.com/dbazure/download/stable/488a1f239235055e34e673291fb8d8c810886f81/code_1.102.3-1753759567_amd64.deb
 dpkg -i code_1.103.0-1754517494_amd64.deb
