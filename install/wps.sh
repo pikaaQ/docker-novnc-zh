1b6c70a78e4fc4f4e7372c1a3acb604ad188bd6a
Mizzle <pikaaq@qq.com>
2025-08-26 01:48:26 +0000

    fix(wps): wps.sh更改下载地址

--- install/wps.sh
+++ install/wps.sh
@@ -1,2 +1,2 @@
-wget https://wps-linux-personal.wpscdn.cn/wps/download/ep/Linux2023/17900/wps-office_12.1.0.17900_amd64.deb
+wget https://repo.debiancn.org/pool/main/w/wps-office/wps-office_12.1.0.17900_amd64.deb
 dpkg -i ps-office_12.1.0.17900_amd64.deb
