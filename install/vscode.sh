ae128e50dba17f800229dedfcf5313957a8c7614
pikaaQ <mizzle@qq.com>
2025-08-26 03:08:44 +0000

    fix: vscode.sh新增安装gnome-keyring.

--- install/vscode.sh
+++ install/vscode.sh
@@ -1,4 +1,4 @@
-apt install xdg-utils -y
+apt install xdg-utils gnome-keyring -y
 
 # code_1.103.0出现输入卡死，不支持google输入法的情况，1.102则没问题
 wget https://vscode.download.prss.microsoft.com/dbazure/download/stable/488a1f239235055e34e673291fb8d8c810886f81/code_1.102.3-1753759567_amd64.deb
